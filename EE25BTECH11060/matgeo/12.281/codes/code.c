0f62c683721df428bc4661609e538bf2d670bf03
V.Namaswi <vajjalanamaswi@gmail.com>
2025-10-12 11:52:05 +0530

    Delete EE25BTECH11060/matgeo/12.281 directory

--- EE25BTECH11060/matgeo/12.281/codes/code.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    double detA = 2.0;
-    int n = 2;
-    double k = 2.0;
-    double det2A = detA * (k * k); // Since n = 2
-    printf("Determinant of 2A is: %.0lf\n", det2A);
-    return 0;
-}
